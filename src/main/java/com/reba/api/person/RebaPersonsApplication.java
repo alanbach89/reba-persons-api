90c7f20a32558d41a0656c32fec70bbd3cbd5fa1
alanbach <alanbach89@gmail.com>
2021-07-31 01:12:51 -0300

    commit inicial generado con https://start.spring.io/

---
+++ src/main/java/com/reba/api/person/RebaPersonsApplication.java
@@ -0,0 +1,13 @@
+package com.reba.api.person;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebaPersonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RebaPersonsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reba/api/person/RebaPersonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reba.api.person;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebaPersonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
