f9650afbfe586d53e0bdebd8c86aa3842545cd8c
alanbach <alanbach89@gmail.com>
2021-08-05 21:59:27 -0300

    agrego db externa

--- src/main/java/com/reba/api/person/database/DatasourceConfig.java
+++ src/main/java/com/reba/api/person/database/DatasourceConfig.java
@@ -1,5 +1,6 @@
 package com.reba.api.person.database;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -10,13 +11,26 @@ import javax.sql.DataSource;
 @Configuration
 @ConfigurationProperties("spring.datasource")
 public class DatasourceConfig {
+
+    @Value("${database.host}")
+    String host;
+
+    @Value("${database.user}")
+    String usernanme;
+
+    @Value("${database.password}")
+    String password;
+
+    @Value("${database.schema}")
+    String schema;
+
     @Bean
     public DataSource datasource() {
         return DataSourceBuilder.create()
                 .driverClassName("com.mysql.cj.jdbc.Driver")
-                .url("jdbc:mysql://localhost:3306/reba_persons?createDatabaseIfNotExist=true")
-                .username("root")
-                .password("")
+                .url("jdbc:mysql://"+host+"/"+schema+"?createDatabaseIfNotExist=true")
+                .username(usernanme)
+                .password(password)
                 .build();
     }
 }
